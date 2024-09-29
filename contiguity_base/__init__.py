31e893c162647e33094eb21547bba925204969f9
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2024-09-28 20:16:47 -0700

    Switch to src layout

--- contiguity_base/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base import connect, Contiguity, Base, Util
-
-__all__ = ['connect', 'Contiguity', 'Base', 'Util']
---
+++ src/contiguity_base/__init__.py
@@ -0,0 +1,4 @@
+from .base import Base, Contiguity, Util, connect
+
+__all__ = ["connect", "Contiguity", "Base", "Util"]
+__version__ = "1.0.0"
